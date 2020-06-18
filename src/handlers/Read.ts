224249070ea5c07170d99ab93247c8963933c058
Dan <dan@dancodes.online>
2020-06-18 10:18:18 +0200

    tests and errors

--- src/handlers/Read.ts
+++ src/handlers/Read.ts
@@ -1,6 +1,17 @@
 import { DenoFs } from "../index.ts";
 import { DenoFile } from "../utils/File.ts";
-
+import { DenoError } from "./Error.ts";
+/**
+ * @class
+ * @name DenoRead
+ * @description Contains functions for reading files
+ * @constructor
+ * @example
+ * ```
+ * const { read } = new DenoFs("./file.png");
+ * read.sync()
+ * ```
+ */
 export class DenoRead {
   filePath: string;
   fs: DenoFs;
@@ -9,8 +20,24 @@ export class DenoRead {
     this.fs = fs;
   }
   sync(options?: {}) {
-    const read = Deno.readFileSync(this.filePath);
-    const file = this.fs.decode(read);
-    return new DenoFile(this.fs, this.filePath, file);
+    try {
+      const read = Deno.readFileSync(this.filePath);
+      const file = this.fs.decode(read);
+      return new DenoFile(this.fs, this.filePath, file);
+    } catch (err) {
+      return new DenoError(err, { msg: "reading " + this.filePath });
+    }
+  }
+  callback(
+    options: {} | ((err: Error | null, file: DenoFile) => void),
+    cb: (err: Error | null, file: DenoFile) => void
+  ) {
+    if (!cb) throw new Error("Callback not specified!");
+    Deno.readFile(this.filePath)
+      .then((read) => {
+        const file = this.fs.decode(read);
+        return cb(null, new DenoFile(this.fs, this.filePath, file));
+      })
+      .catch((err) => new DenoError(err, { msg: "reading " + this.filePath }));
   }
 }
