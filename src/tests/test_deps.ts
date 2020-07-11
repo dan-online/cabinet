f33142ec3af3662896a788cdc0b8d65570906e26
DanCodes <dan@dancodes.online>
2020-07-11 23:33:15 +0200

    fix test

--- src/tests/test_deps.ts
+++ src/tests/test_deps.ts
@@ -1,2 +1,2 @@
 // Add your test dependencies in here
-export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.59.0/testing/asserts.ts";
